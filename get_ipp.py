9e6a6666bceee9fe93dd8661f951cb5ae0468355
minggLu <mylu@bu.edu>
2015-11-29 23:01:19 -0500

    try figure out how to decode chunked encoding data

--- get_ipp.py
+++ get_ipp.py
@@ -6,9 +6,10 @@ def main():
     name = "printer6.pcapng"
     pc = pcap.pcap(name)
     filter = 'tcp dst port 631'
+# filter = 'tcp dst port 631 and tcp[((tcp[12:1] & 0xf0) >> 2):4]=0x02000006'
     pc.setfilter(filter)
     decode = { pcap.DLT_LOOP:dpkt.loopback.Loopback,
-               pcap.DLT_NULL:dpkt.loopback.Loopback,
+               #pcap.DLT_NULL:dpkt.loopback.Loopback,
                pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
     try:
         for ts, pkt in pc:
