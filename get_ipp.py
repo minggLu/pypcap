32765782f027f158dd6a1be5db11299fe6aa94a4
minggLu <mylu@bu.edu>
2015-11-29 20:34:21 -0500

    add sample pcap file and naive approach to get ipp

---
+++ get_ipp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import dpkt, pcap
+
+def main():
+    name = "printer6.pcapng"
+    pc = pcap.pcap(name)
+    filter = 'tcp dst port 631'
+    pc.setfilter(filter)
+    decode = { pcap.DLT_LOOP:dpkt.loopback.Loopback,
+               pcap.DLT_NULL:dpkt.loopback.Loopback,
+               pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
+    try:
+        for ts, pkt in pc:
+            print ts, `decode(pkt)`
+    except KeyboardInterrupt:
+        print 'Stoped' 
+        
+if __name__ == '__main__':
+    main() 
