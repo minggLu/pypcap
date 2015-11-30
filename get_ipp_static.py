8488b7a73492ddcbcb2cffce0f07985e66743ff3
minggLu <mylu@bu.edu>
2015-11-30 17:09:53 -0500

    very close to be done
    - add sample ipp output
    - need to figure out an generic way to rip off the last 7 bytes of the encoding
    - the beginning has a '\r\n' for no reason......

--- get_ipp_static.py
+++ get_ipp_static.py
@@ -4,60 +4,67 @@ import dpkt, pcap
 
 def main():
     name = "printer6.pcapng"
+    pc = pcap.pcap(name)
     begin = 0
     end = 0
-    pc = pcap.pcap(name)
+    pkt_list = []
+    ipp_list = []
+    output = ''
+    # IPP protocol typically use destination port 631
     filter = 'tcp dst port 631'
-#    filter = 'tcp dst port 631 and tcp[((tcp[12:1] & 0xf0) >> 2):4]=0x02000006'
     pc.setfilter(filter)
     decode = { pcap.DLT_LOOP:dpkt.loopback.Loopback,
                pcap.DLT_NULL:dpkt.loopback.Loopback,
                pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
     try:
-        pc_enum = enumerate(iter(pc))
-        pkt_list = []
+        index = 0
+        legit = False
         payload = ''
         hex_payload = ''
-        index = 0
-        ipp_list = []
-        output = ''
-        legit = False
-# the last 4 character in the chunked data is \r\n\r\n (or 0d0a0d0a)
         for ts, pkt in pc:
             payload = decode(pkt).data.data.data
             hex_payload = payload.encode("hex") 
             pkt_list.append(hex_payload)
+            # beginning of Sent-Document
+            # IPP version: \x02\x00 (2-byte)
+            # Operation-id = \x00\x06 (2-byte)
             if begin == 0 and payload[:4] == '\x02\x00\x00\x06':
                 begin = index
-            
+
+            # end of chunked encoding is '0\r\n\r\n' (or 30d0a0d0a)
             if begin != 0 and end == 0 and hex_payload[-10:] == '300d0a0d0a':
                 end = index
 
             index += 1
-        
-        for ii in range(begin, end):
+ 
+        for ii in range(begin, end+1):
             if len(ipp_list) == 0:
                 ipp_list.append(pkt_list[ii])
                 continue
 
-# the ending iter is not getting incremented
-            legit = pkt_list[ii] != ipp_list[0] and pkt_list[ii] != '' and pkt_list[ii][-4:] != '0d0a'
+            # the ending iter is not getting incremented
+            legit = pkt_list[ii] != ipp_list[0]
 
+            # rip off repeating data, '' and '\r\n'
+            # except the ending chunk where ends with '300d0a0d0a'
             for jj in range(1, len(ipp_list)):
-                legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != '' and pkt_list[ii][-4:] != '0d0a'
+                legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != '' and (pkt_list[ii][-4:] != '0d0a' or pkt_list[ii][-10:] == '300d0a0d0a')
 
             if legit == True:
                 ipp_list.append(pkt_list[ii])
                 legit = False
 
-        for kk in range(0, len(ipp_list)):
+        # the first item is the request, data starts on the 2nd piece
+        for kk in range(1, len(ipp_list)):
             output += ipp_list[kk]
 
+        output = output[4:-14]
+        f = open("ipp.txt", "w")
+        f.write(output)
         print output
-        import pdb; pdb.set_trace()
 
     except KeyboardInterrupt:
-        print 'Stoped' 
-        
+        print 'Stoped'
+
 if __name__ == '__main__':
-    main() 
+    main()
