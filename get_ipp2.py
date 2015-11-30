65d3da3eaaaf51bd811fcb5498d0ee5f858a78de
minggLu <mylu@bu.edu>
2015-11-30 01:15:42 -0500

    update on decoding
    - get range for ipp operation-id==6 packet
    - get everything within chunked message
    - still some leftover encoding need to be removed '\r\n' and empty stuff
    - still exists repeating pattern

--- get_ipp2.py
+++ get_ipp2.py
@@ -15,22 +15,28 @@ def main():
                pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
     try:
         pc_enum = enumerate(iter(pc))
+        pkt_list = []
         payload = ''
         hex_payload = ''
+        index = 0
+        output = ''
 # the last 4 character in the chunked data is \r\n\r\n (or 0d0a0d0a)
         for ts, pkt in pc:
             payload = decode(pkt).data.data.data
             hex_payload = payload.encode("hex") 
-            if payload[:4] == '\x02\x00\x00\x06':
-                print payload
-                import pdb; pdb.set_trace()
-                begin = ts 
+            pkt_list.append(hex_payload)
+            if begin == 0 and payload[:4] == '\x02\x00\x00\x06':
+                begin = index
             
             if begin != 0 and end == 0 and hex_payload[-10:] == '300d0a0d0a':
-                print payload
-                end = ts
+                end = index
 
-            print ts, `decode(pkt)`
+            index += 1
+        
+        for ii in range(begin, end+1):
+            output += pkt_list[ii]
+
+        print output
 
     except KeyboardInterrupt:
         print 'Stoped' 
