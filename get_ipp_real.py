e78bc1d42dc414d9ba06cedea9161b534e0b948f
minggLu <mylu@bu.edu>
2015-12-05 02:21:02 -0500

    updated data packet processing

--- get_ipp_real.py
+++ get_ipp_real.py
@@ -8,31 +8,30 @@ def usage():
     print >>sys.stderr, 'usage: %s [-i device] [pattern]' % sys.argv[0]
     sys.exit(1)
 
-def __my_handler(ts,pkt, pc, ipp_list):
+def get_data(ts,pkt, pc, pkt_list):
 
     decode = { pcap.DLT_LOOP:dpkt.loopback.Loopback,
                pcap.DLT_NULL:dpkt.loopback.Loopback,
                pcap.DLT_EN10MB:dpkt.ethernet.Ethernet }[pc.datalink()]
 
-    #import pdb; pdb.set_trace()
     hex_payload = decode(pkt).data.data.data.encode("hex") 
     print hex_payload 
 
-    if len(ipp_list) == 0 and hex_payload[:20] == "1f8b0800000000000003":
-        ipp_list.append(hex_payload)
+    # the gzip file signature is \x1f\x8b\x08
+    if len(pkt_list) == 0 and hex_payload[:8] == "02000006":
+        pkt_list.append(hex_payload)
         print 'add first packet'
         print hex_payload
 
     # end of chunked encoding is '0\r\n\r\n' (or 30d0a0d0a)
-    elif len(ipp_list) != 0 and hex_payload[-10:] != '300d0a0d0a':
-        ipp_list.append(hex_payload)
+    elif len(pkt_list) != 0 and hex_payload[-10:] != '300d0a0d0a':
+        pkt_list.append(hex_payload)
         print 'add last packet'
         print hex_payload
 
-    elif len(ipp_list) != 0 and hex_payload[-10:] == '300d0a0d0a':
+    elif len(pkt_list) != 0 and hex_payload[-10:] == '300d0a0d0a':
         print hex_payload
         raise KeyboardInterrupt
-        #raise Exception()
 
 
 def main():
@@ -42,40 +41,75 @@ def main():
         if o == '-i': name = a
         else: usage()
         
-    pc = pcap.pcap(name, immediate=True)
-    ipp_list = []
     output = ''
-    # IPP protocol typically use destination port 631
+
+    # starts capturing with pypcap library
+    pc = pcap.pcap(name, immediate=True)
+    pkt_list = []
+    ipp_list = []
+    # IPP protocol uses TCP with destination port 631
+    # the filter follows libpcap syntax
     filter = 'tcp dst port 631'
     pc.setfilter(filter)
     try:
-        index = 0
-        legit = False
-        myfile = open('realtime.txt', 'w')
         print 'listening on %s: %s' % (pc.name, pc.filter)
-        #import pdb; pdb.set_trace()
-        pc.loop(-1, __my_handler, pc, ipp_list)
+        # pc.loop is a function that process a packet
+        # with a user callback, in this case is get_data 
+        # get_data looks for the ipp packets that
+        # contains document data
+        pc.loop(-1, get_ipp, pc, pkt_list)
 
-#            if begin != 0 and end != 0:
-#                for ii in range(begin, end+1):
-#                    if len(ipp_list) == 0:
-#                        myfile.write(pkt_list[ii])
-#                #        ipp_list.append(pkt_list[ii])
-#                
-#                        continue
-#
-#                    # preset
-#                    legit = True
-#                    # rip off repeating data, '' and '\r\n'
-#                    # except the ending chunk where ends with '300d0a0d0a'
-#                    for jj in range(0, len(ipp_list)):
-#                        legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != ''
-#                    if pkt_list[ii][-4:] == '0d0a':
-#                       legit = legit and pkt_list[ii][-10:] == '300d0a0d0a'
-#
-#                    if legit == True:
-#                        myfile.write(pkt_list[ii])
-#                #        ipp_list.append(pkt_list[ii])
+
+        # process tcp segments between beginning of operation-id 6 
+        # and end of chunking
+        for ii in range(begin, end+1):
+            if len(ipp_list) == 0:
+                ipp_list.append(pkt_list[ii])
+                continue
+
+            # pre-set
+            legit = True
+            # rip off repeating data, '' and '\r\n'
+            # check to avoid repeating data segment
+            # except the ending chunk where ends with '300d0a0d0a'
+            # tcp segments with the chunked data all have length = 1448 byte
+            for jj in range(0, len(ipp_list)):
+                legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != '' and (len(pkt_list[ii]) == 2896 or ii == end)
+
+            if pkt_list[ii][-4:] == '0d0a':
+               legit = legit and pkt_list[ii][-10:] == '300d0a0d0a'
+
+            if legit == True:
+                ipp_list.append(pkt_list[ii])
+
+        # write data in to output string
+        for kk in range(1, len(ipp_list)):
+            output += ipp_list[kk]
+
+        # rip off end of chunk encoding hex bits
+        output = output[:-14]
+
+        # remove random '\r\n' + some bytes + '\r\n' in output
+        first = 0
+        second = 0
+        start = 0
+        n = 0
+        while output.find('0d0a', start) > -1:
+            first = output.find('0d0a', start)
+            second = output.find('0d0a', first+1)
+            import pdb; pdb.set_trace()
+            if second > 0 and second - first <= 64:
+                pattern = output[first:second+4]
+                output = ''.join(output.split(pattern))
+                start = second + 5
+                first = 0
+                second = 0
+        f = open("ipp.txt", "w")
+        f.write(output)
+        f.close()
+        print output
+        print len(output)
+        import pdb; pdb.set_trace()
 
     except KeyboardInterrupt:
         nrecv, ndrop, nifdrop = pc.stats()
