6ab9d99f699e42679b8045545a44d22f91fb9564
minggLu <mylu@bu.edu>
2015-12-05 09:50:50 -0500

    update on extracting ipp packets
    - check for starting 4 bytes '0200000?' if ? != 6, exclude
    - check for lasting 4 bytes '0d0a0d0a', because all the operation-id != 6 ipp packets has 2 tcp segments, that are not necessarily sent right next to each other. But the first one all had '\r\n\r\n' as ending and the second one always have '0200000?' as beginning
    - check for random '\r\n' in the middle of the packet and get rid of them
    - check for random short (like <10 bytes) tcp segments in the middle and exclude

--- get_ipp_static.py
+++ get_ipp_static.py
@@ -7,12 +7,12 @@ import dpkt, pcap
 #        return text.replace(middle, alternative)
 
 def main():
-    name = "printer4.pcapng"
+    name = "printer3.pcapng"
     pc = pcap.pcap(name)
     begin = 0
     end = 0
     pkt_list = []
-    ipp_list = []
+    ipp_list = ['']
     data_list = []
     output = ''
     # IPP protocol typically use destination port 631
@@ -43,19 +43,22 @@ def main():
             index += 1
  
         for ii in range(begin, end+1):
-            if len(ipp_list) == 0:
-                ipp_list.append(pkt_list[ii])
-                continue
+#            if len(ipp_list) == 0:
+#                ipp_list.append(pkt_list[ii])
+#                continue
 
             # pre-set
             legit = True
             # rip off repeating data, '' and '\r\n'
             # except the ending chunk where ends with '300d0a0d0a'
-            for jj in range(0, len(ipp_list)):
-                legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != '' and (len(pkt_list[ii]) == 2896 or ii == end)
-
-            if pkt_list[ii][-4:] == '0d0a':
-               legit = legit and pkt_list[ii][-10:] == '300d0a0d0a'
+            # check for length of data segment == 1446*2 
+            # check for header != 0200000x, and delete the segment right before
+            for jj in range(0, max(1, len(ipp_list))):
+                legit = legit and pkt_list[ii] != ipp_list[jj] and pkt_list[ii] != '' and len(pkt_list[ii]) >= 128
+            if pkt_list[ii][-4:] == '0d0a': 
+                legit = legit and pkt_list[ii][-10:] == '300d0a0d0a'
+            elif pkt_list[ii][:7] == '0200000':
+                legit = False
 
             if legit == True:
                 ipp_list.append(pkt_list[ii])
@@ -63,7 +66,6 @@ def main():
         for kk in range(1, len(ipp_list)):
             output += ipp_list[kk]
 
-        #output = output.replace('0d0a3830300d0a', '')
         output = output[:-14]
 
         # actually write a generic solution
@@ -75,13 +77,22 @@ def main():
             first = output.find('0d0a', start)
             second = output.find('0d0a', first+1)
             import pdb; pdb.set_trace()
-            if second - first == 10:
+
+            if second - first <= 64 and second > 0:
                 pattern = output[first:second+4]
                 output = ''.join(output.split(pattern))
                 start = second + 5
                 first = 0
                 second = 0
-        f = open("ipp1txt", "w")
+            else:
+                pattern = output[first:first+4]
+                output = ''.join(output.split(pattern))
+                import pdb; pdb.set_trace()
+                start = first + 5
+                first = 0
+                second = 0
+
+        f = open("ipp3.txt", "w")
         f.write(output)
         f.close()
         print output
